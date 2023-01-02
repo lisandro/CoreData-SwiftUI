ea591989f56903ee12cf2c9ee73ac99bd3c60c03
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2023-01-02 13:17:54 -0300

    Initial Commit

---
+++ MyRidesOrDies/MyRidesOrDiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRidesOrDiesApp.swift
+//  MyRidesOrDies
+//
+//  Created by Lisandro Falconi on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyRidesOrDiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
