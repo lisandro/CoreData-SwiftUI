60e02923a3c26ede11657ce39b0b0d9fed4eaa1a
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2023-01-02 13:28:17 -0300

    feat: Added Contact data model

---
+++ MyRidesOrDies/Model/Contact.swift
@@ -0,0 +1,25 @@
+//
+//  Contact.swift
+//  MyRidesOrDies
+//
+//  Created by Lisandro Falconi on 02/01/2023.
+//
+
+import Foundation
+import CoreData
+
+final class Contact: NSManagedObject {
+    
+    @NSManaged var dob: Date
+    @NSManaged var email: String
+    @NSManaged var isFavourite: Bool
+    @NSManaged var name: String
+    @NSManaged var notes: String
+    @NSManaged var phoneNumber: String
+    
+    override func awakeFromInsert() {
+        super.awakeFromInsert()
+        setPrimitiveValue(Date.now, forKey: "dob")
+        setPrimitiveValue(false, forKey: "isFavourite")
+    }
+}
